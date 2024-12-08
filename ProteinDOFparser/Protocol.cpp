2c3c08694eec87899b2c7a0e40f5a3cea08d462a
MuWang <18640057101@163.com>
2024-12-08 11:08:24 +0800

    update protocol

--- ProteinDOFparser/Protocol.cpp
+++ ProteinDOFparser/Protocol.cpp
@@ -9,3 +9,14 @@
 /**
  * Protocol implementation
  */
+
+Protocol::Protocol(Pose& pose, Selector& selector) {
+    this->pose = pose;
+    this->selector = selector;
+    this->init();
+    this->run();
+}
+
+void Protocol::init() {}
+
+void Protocol::run() {}
--- ProteinDOFparser/Protocol.h
+++ ProteinDOFparser/Protocol.h
@@ -8,8 +8,17 @@
 #define _PROTOCOL_H
 
 #include "ProteinDOFparser_common.h"
+#include "Pose.h"
+#include "Selector.h"
 
 class Protocol {
+public:
+    Pose pose;
+    Selector selector;
+    
+    Protocol(Pose& pose, Selector& selector);
+    void init();
+    void run();
 };
 
 #endif //_PROTOCOL_H
